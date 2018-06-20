9b2382dc18faecd1056be74370865a259ade6c70
liuchongyu <liuchongyu007@163.com>
2018-06-20 09:53:42 +0800

    测试提交

--- src/test/java/example/OneTest.java
+++ src/test/java/example/OneTest.java
@@ -7,7 +7,7 @@ public class OneTest {
   @Test
   public void testFoo() throws Exception {
     One one = new One();
-    //Test foo 提价
+    //Test foo 提交
     assertEquals("foo", one.foo());
   }
 
